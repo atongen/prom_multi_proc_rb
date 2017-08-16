7e5b4cc12cf772ed1693acea8ad5a877ef2ab568
Andrew Tongen <atongen@gmail.com>
2017-08-16 15:14:21 -0500

    Fix prefix

--- lib/prom_multi_proc/rails.rb
+++ lib/prom_multi_proc/rails.rb
@@ -5,7 +5,8 @@ module PromMultiProc
       socket = ENV.fetch("PROM_MULTI_PROC_SOCKET", ::Rails.root.join("tmp/sockets/metrics.sock").to_s)
 
       program_name = File.basename($PROGRAM_NAME)
-      prefix ||= ::Rails.application.class.name.underscore.split("/").first + "_"
+      app_name = ::Rails.application.class.name.underscore.split("/").first
+      prefix ||= "#{app_name}_"
 
       if ENV.key?("PROM_MULTI_PROC_BATCH_SIZE")
         batch_size = ENV["PROM_MULTI_PROC_BATCH_SIZE"].to_i
@@ -23,7 +24,7 @@ module PromMultiProc
         validate = false
       end
 
-      ::Rails.logger.error("Setting up metrics for #{program_name}, batch size: #{batch_size}, validate: #{validate}")
+      ::Rails.logger.error("Setting up prom_multi_proc for #{app_name}-#{program_name}, batch size: #{batch_size}, validate: #{validate}")
 
       Base.new(
         prefix: prefix,
